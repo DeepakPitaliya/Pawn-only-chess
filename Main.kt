ac1ace346ba54cc90e3d703d2242d55d46457cbc
Deepak Pitaliya <pitaliya.d@gmail.com>
2023-03-07 12:52:38 +0530

    Stage2 completed

--- Main.kt
+++ Main.kt
@@ -1,41 +1,48 @@
-fun main(args: Array<String>) {
-    createBoard()
+fun main() {
+    printChessBoard()
+    makeMove()
 }
 
-fun createBoard() {
+var player1 = ""
+var player2 = ""
+fun printChessBoard() {
+    val separator = "  " + "+---".repeat(8) + "+"
+    val arr = Array(8) { Array(8) { "   " } }
+    for (i in 0..7) {
+        arr[1][i] = " W "
+        arr[6][i] = " B "
+    }
     println("Pawns-Only Chess")
-    val hLine = "  +---+---+---+---+---+---+---+---+"
-    val vLine = "|   |   |   |   |   |   |   |   |"
-    for (i in 0..16) {
-        if (i == 3) {
-            println("7 | B | B | B | B | B | B | B | B |")
-            continue
+    println("First Player's name:")
+    player1 = readln()
+    println("Second Player's name:")
+    player2 = readln()
+    println(separator)
+    for (i in 8 downTo 1) {
+        print("$i ")
+        for (j in 8 downTo 1) {
+            print("|" + arr[i - 1][j - 1])
         }
-        if (i == 13) {println("2 | W | W | W | W | W | W | W | W |")
-            continue
-        }
-        if (i % 2 == 0) println(hLine) else println("${9 - (i + 1)/2} " + vLine)
+        println("|")
+        println(separator)
     }
     println("    a   b   c   d   e   f   g   h")
 }
-/*
-Pawns-Only Chess
-  +---+---+---+---+---+---+---+---+
-8 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-7 | B | B | B | B | B | B | B | B |
-  +---+---+---+---+---+---+---+---+
-6 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-5 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-4 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-3 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-2 | W | W | W | W | W | W | W | W |
-  +---+---+---+---+---+---+---+---+
-1 |   |   |   |   |   |   |   |   |
-  +---+---+---+---+---+---+---+---+
-    a   b   c   d   e   f   g   h
- */
+
+fun makeMove() {
+    val validMove = Regex("[a-h][1-8][a-h][1-8]")
+    var player = player1
+    while (true) {
+        println("$player's turn:")
+        val inputMove = readln()
+        if (inputMove == "exit") {
+            println("Bye!")
+            break
+        }
+        if (validMove.matches(inputMove)) {
+            player = if (player == player1) player2 else player1
+        } else {
+            println("Invalid Input")
+        }
+    }
+}
