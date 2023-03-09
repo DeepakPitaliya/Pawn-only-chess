2add75eaecca74c0991d4f328a4332df9b801d65
Deepak Pitaliya <pitaliya.d@gmail.com>
2023-03-09 10:55:04 +0530

    Stage3 re done

--- Main.kt
+++ Main.kt
@@ -8,7 +8,7 @@ fun main() {
     player2 = readln()
     printChessBoard(chessBoard)
 
-    makeMove(chessBoard)
+    play(chessBoard)
 }
 
 var player1 = ""
@@ -24,7 +24,6 @@ fun createInitialBoard(): Array<Array<String>> {
 
 fun printChessBoard(chessBoard: Array<Array<String>>) {
     val separator = "  " + "+---".repeat(8) + "+"
-
     println(separator)
     for (i in 8 downTo 1) {
         print("$i ")
@@ -37,7 +36,7 @@ fun printChessBoard(chessBoard: Array<Array<String>>) {
     println("    a   b   c   d   e   f   g   h")
 }
 
-fun makeMove(chessBoard: Array<Array<String>>) {
+fun play(chessBoard: Array<Array<String>>) {
     val validMove = Regex("[a-h][1-8][a-h][1-8]")
     var player = player1
     while (true) {
@@ -48,65 +47,38 @@ fun makeMove(chessBoard: Array<Array<String>>) {
             break
         }
         if (validMove.matches(inputMove)) {
-            if (player == player1) {
-                if (moveWhite(chessBoard, inputMove)) player = player2
-            } else {
-                if(moveBlack(chessBoard, inputMove)) player = player1
+            if (movePawn(chessBoard, inputMove, player)) {
+                player = if (player == player1) player2 else player1
             }
         }
     }
 }
+fun movePawn(chessBoard: Array<Array<String>>, inputMove: String, player: String): Boolean {
+    val row1 = inputMove[1].digitToInt() - 1
+    val col1 = inputMove[0].code - 97
+    val row2 = inputMove[3].digitToInt() - 1
+    val col2 = inputMove[2].code - 97
 
-//player move input is col,row,col,row whereas chessBoard takes row,col,row,col
-// convert player move to game board coordinates e.g. b2b4 = [1, 1, 3, 1]
-// corresponding to game board's positions chessBoard[1][1] and chessBoard[3][1]
+    val step = if (player == player1) 1 else -1
+    val pawnColor = if (player == player1) " W " else " B "
+    val startingRow = if (player == player1) 1 else 6
 
-fun getCoordinates(s: String): Array<Int> {
-    return arrayOf(s[1].digitToInt() - 1, s[0].code - 97, s[3].digitToInt() - 1, s[2].code - 97)
-}
-
-fun moveWhite(chessBoard: Array<Array<String>>, inputMove: String): Boolean {
-    val coord = getCoordinates(inputMove)
     var isValid = false
-    if (chessBoard[coord[0]][coord[1]] != " W ") {
-        println("No white pawn at ${inputMove.subSequence(0, 2)}")
-    } else if(inputMove.subSequence(0, 1) != inputMove.subSequence(2, 3)) {
-        println("Invalid Input")
-    } else if (coord[0] == 1 && !((coord[2] - coord[0]) == 1 || (coord[2] - coord[0]) == 2)) {
-        println("Invalid Input")
-    } else if (coord[0] == 1 && chessBoard[2][coord[1]] != "   ") {
-        println("Invalid Input")
-    } else if (coord[0] > 1 && coord[2] - coord[0] != 1) {
-        println("Invalid Input")
-    } else if (chessBoard[coord[2]][coord[3]] != "   ") {
+
+    if (chessBoard[row1][col1] != pawnColor) {
+        println("No ${if (player == player1) "white" else "black"} pawn at ${inputMove.subSequence(0, 2)}")
+    } else if (
+        (col1 != col2)
+        || (row1 == 1 && (row2 - row1 > 2 || row2 - row1 <= 0))
+        || (row1 == 6 && (row1 - row2 > 2 || row1 - row2 <= 0))
+        || chessBoard[row1 + step][col1] != "   "
+        || (row1 != startingRow && row2 - row1 != step)
+        || (chessBoard[row2][col2] != "   ")
+    ) {
         println("Invalid Input")
     } else {
-        chessBoard[coord[0]][coord[1]] = "   "
-        chessBoard[coord[2]][coord[3]] = " W "
-        printChessBoard(chessBoard)
-        isValid = true
-    }
-    return isValid
-}
-
-fun moveBlack(chessBoard: Array<Array<String>>, inputMove: String): Boolean {
-    val coord = getCoordinates(inputMove)
-    var isValid = false
-    if (chessBoard[coord[0]][coord[1]] != " B ") {
-        println("No black pawn at ${inputMove.subSequence(0, 2)}")
-    } else if(inputMove.subSequence(0, 1) != inputMove.subSequence(2, 3)) {
-        println("Invalid Input")
-    } else if (coord[0] == 6 && !((coord[0] - coord[2]) == 1 || (coord[0] - coord[2]) == 2)) {
-        println("Invalid Input")
-    } else if (coord[0] == 6 && chessBoard[5][coord[1]] != "   ") {
-        println("Invalid Input")
-    } else if (coord[0] < 6 && coord[0] - coord[2] != 1) {
-        println("Invalid Input")
-    } else if (chessBoard[coord[2]][coord[3]] != "   ") {
-        println("Invalid Input")
-    } else {
-        chessBoard[coord[0]][coord[1]] = "   "
-        chessBoard[coord[2]][coord[3]] = " B "
+        chessBoard[row1][col1] = "   "
+        chessBoard[row2][col2] = pawnColor
         printChessBoard(chessBoard)
         isValid = true
     }
